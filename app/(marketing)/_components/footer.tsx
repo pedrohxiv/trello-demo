7329c6fb3b97787323296f7525ad5d73d9e1e1d3
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2023-12-08 18:26:32 -0300

    Marketing page created

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+
+export const Footer = () => {
+  return (
+    <div className="fixed bottom-0 w-full px-4 py-2 border-t bg-slate-100">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <p className="text-sm text-neutral-400">
+          &copy; {new Date().getFullYear()} All rights Reserved.
+        </p>
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+          <Button size="sm" variant="ghost">
+            Privacy Policy
+          </Button>
+          <Button size="sm" variant="ghost">
+            Terms of Service
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
